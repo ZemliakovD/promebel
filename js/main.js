0098c6dc48a3d3296d770a4d27b1a3ce59ddf6f9
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-10-15 23:41:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    $('.rooms-cards').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+
+      $('.mebel-content-gallery').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+
+      $('.about-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+
+});
