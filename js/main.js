c46028a1d44d97867f13c7efd8d92574f8bf339a
Dmitri Zemliakov <112906798+ZemliakovD@users.noreply.github.com>
2023-10-18 00:25:12 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="rooms-prev"><i class="fa-solid fa-arrow-left fa-beat" style="color: #000000;"></i></button>',
+        nextArrow: '<button type="button" class="rooms-next"><i class="fa-solid fa-arrow-right fa-beat" style="color: #000000;"></i></button>'
       });
 
       $('.mebel-content-gallery').slick({
@@ -12,13 +14,17 @@ $(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="mebel-prev"><i class="fa-solid fa-arrow-left fa-beat" style="color: #000000;"></i></button>',
+        nextArrow: '<button type="button" class="mebel-next"><i class="fa-solid fa-arrow-right fa-beat" style="color: #000000;"></i></button>'
       });
 
       $('.about-slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="about-prev"><i class="fa-solid fa-arrow-left fa-beat" style="color: #000000;"></i></button>',
+        nextArrow: '<button type="button" class="about-next"><i class="fa-solid fa-arrow-right fa-beat" style="color: #000000;"></i></button>'
       });
 
 });
